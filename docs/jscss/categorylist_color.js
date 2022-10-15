a5c20f80d9d3d10925727f6a18df534d73581577
masapi61 <92568108+masapi61@users.noreply.github.com>
2022-10-16 02:15:15 +0900

    レイアウト調整

--- docs/jscss/categorylist_color.js
+++ docs/jscss/categorylist_color.js
@@ -1,11 +1,11 @@
 var category_color = {
-    'cat-0': 'red',
-    'cat-1': 'lightslategray',
-    'cat-2': 'green',
+    'cat-0': 'rgb(255, 143, 143)',
+    'cat-1': 'gainsboro',
+    'cat-2': 'rgb(117, 214, 161)',
     'cat-3': 'yellow',
-    'cat-4': 'purple',
+    'cat-4': 'rgb(204, 179, 255)',
     'cat-5': 'orange',
-    'cat-6': 'brown',
+    'cat-6': 'peachpuff',
     'cat-7': 'aqua',
     'cat-8': 'lime',
     'cat-9': 'pink'
