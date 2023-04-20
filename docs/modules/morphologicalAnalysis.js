d910af715a469a068e306bd309dac874a1fa9662
shintaro fukuda <74655911+shunafuku@users.noreply.github.com>
2023-04-20 19:01:10 +0900

    update

--- docs/modules/morphologicalAnalysis.js
+++ docs/modules/morphologicalAnalysis.js
@@ -18,4 +18,20 @@ async function morphologicalAnalysis(text) {
     });
 }
 
+//kuromoji形態素解析(解析文章)
+function promise_analysis(text) {
+    //kuromoji必要変数
+    const ids = [];
+    const names = [];
+    const DICT_PATH = "./dict";
+    return new Promise((resolve) => {
+        // Kuromoji
+        kuromoji.builder({ dicPath: DICT_PATH }).build((err, tokenizer) => {
+            const tokens = tokenizer.tokenize(text);// 解析データの取得
+            console.log("形態素解析結果", tokens);
 
+            //console.log(words);
+            return resolve(tokens);
+        });
+    });
+}
--- docs/modules/search.js
+++ docs/modules/search.js
@@ -3,3 +3,23 @@
 //曖昧検索
 
 //複合語検索
+
+
+//mediawiki_API 前方一致(検索ワード,結果取得上限数)
+function promise_get_action_wbsearchentities(word, limit) {
+    return new Promise((resolve) => {
+        const urlw = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search=" + word + "&limit=" + limit + "&language=ja&format=json&origin=*";
+        fetch(urlw)
+            .then(function (response) { return resolve(response.json()); })
+            .catch(function (error) { console.log(error); });
+    })
+}
+//wikibase_API あいまい(検索ワード,結果取得上限数)
+function promise_get_list_search(word, limit) {
+    return new Promise((resolve) => {
+        const urlw = "https://www.wikidata.org/w/api.php?action=query&list=search&srsearch=" + word + "&srlimit=" + limit + "&sroffset=0&format=json&origin=*";
+        fetch(urlw)
+            .then(function (response) { return resolve(response.json()); })
+            .catch(function (error) { console.log(error); });
+    })
+}
