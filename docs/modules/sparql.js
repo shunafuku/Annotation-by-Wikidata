ee0a2461dc393fc49608288103b60f70342e815c
shintaro fukuda <74655911+shunafuku@users.noreply.github.com>
2023-04-21 18:55:19 +0900

    move sparqlQueryFunction

--- docs/modules/sparql.js
+++ docs/modules/sparql.js
@@ -1,4 +1,4 @@
-export async function fetchSparql(endpointUrl, sparqlQuery) {
+async function fetchSparql(endpointUrl, sparqlQuery) {
     const fullUrl = endpointUrl + "?query=" + encodeURIComponent(sparqlQuery);
     const headers = {
         Accept: "application/sparql-results+json",
@@ -7,3 +7,39 @@ export async function fetchSparql(endpointUrl, sparqlQuery) {
         body.json()
     );
 };
+
+//wikidata sparql endpoint(wikidataID:Q???)
+function sparql_query(ids) {
+    return new Promise((resolve) => {
+        //SPARQLエンドポイントの初期設定
+        const endpoint = "https://query.wikidata.org/sparql";
+
+        //SPARQLクエリの発行
+        //idsにsearchAPIで返ってきた「Q???」を全て代入
+        const query = 'PREFIX wdt: <http://www.wikidata.org/prop/direct/> \n'
+            + 'PREFIX wd: <http://www.wikidata.org/entity/> \n'
+            + 'select  DISTINCT ?s ?sLabel ?o ?oLabel \n'
+            + 'where{ \n'
+            + 'VALUES ?s {' + ids + '} \n'
+            + '?s wdt:P31*/wdt:P279* ?o.  \n'
+            + 'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }\n'
+            + '}ORDER BY ASC (?s)'
+        //console.log(query)
+
+        //SPARQLクエリの実行（sparql.jsで定義している関数を利用）
+        qr = sendQuery(endpoint, query);
+
+        qr.fail(//クエリが失敗したときの処理
+            function (xhr, textStatus, thrownError) {
+                //alert("Error: A '" + textStatus + "' occurred.");
+            }
+        );
+        qr.done(//クエリが成功したときの処理
+            function (d) {
+                //console.log(d);
+                return resolve(d.results.bindings);
+            }
+        );
+    })
+
+}
