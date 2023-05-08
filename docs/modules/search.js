512db38da3283f52db704cb685055309b8b8817e
shintaro fukuda <74655911+shunafuku@users.noreply.github.com>
2023-05-08 19:43:38 +0900

    update

--- docs/modules/search.js
+++ docs/modules/search.js
@@ -3,6 +3,33 @@
 //曖昧検索
 
 //複合語検索
+const inputTextArray = [{word:"先進",flag:false},{word:"国",flag:false},{word:"首脳",flag:false},{word:"会議",flag:false}];
+const answerTextArray = ["国首脳", "先進","国", "首脳", "会議"]
+function exists(word){
+    return answerTextArray.includes(word);
+}
+
+for(let compoundWordNum = inputTextArray.length; 0 < compoundWordNum; compoundWordNum--){
+    const patternCount = inputTextArray.length - compoundWordNum + 1;
+    console.log("neko-------------------")
+    for(let i = 0; i < patternCount; i++){
+        const flagArray = inputTextArray.slice(i, i + compoundWordNum).map(x => x["flag"])
+        //flagが全部falseなら実行
+        if(){
+                    const compoundWord =  inputTextArray.slice(i, i + compoundWordNum).map(x => x["word"]).join("");
+        console.log(flagArray);
+        console.log(compoundWord);
+        }
+
+        
+        if(exists(compoundWord)){
+            for(let j = i; j < i + compoundWordNum; j++){
+                inputTextArray[j]["flag"] = true;
+            }
+        }
+        
+    }
+}
 
 
 //mediawiki_API 前方一致(検索ワード,結果取得上限数)
